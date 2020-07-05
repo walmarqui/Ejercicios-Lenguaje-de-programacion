0e8e6c1e2fad06a2dc74ef1fae500148efca251e
walter <walmarqui@gmail.com>
2020-07-04 22:21:50 -0600

    agregando tarea 1: calculadora

---
+++ EjerciciosVC/ejercicio-6-calculadora/calculadora.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 0;
+    int b = 0;
+
+    cout<<"****CALCULADORA****\n";
+
+    cout<<" Ingrese el valor de a:"<<endl;
+    cin>>a;
+
+    cout<<"Ingrese el valor de b:"<<endl;
+    cin>>b;
+
+    cout<<"La suma de a + b es:"<<a+b<<endl;
+    cout<<"La resta de a - b es:"<<a-b<<endl;
+    cout<<"La multiplicacion de a * b es:"<<a*b<<endl;
+    cout<<"La divicion de a / b es:"<<a/b<<endl;
+    return 0;
+}
