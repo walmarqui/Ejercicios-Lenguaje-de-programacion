d8f493b5587b33cd2a57c83d4f74d49a587446e8
walter <walmarqui@gmail.com>
2020-07-18 09:07:28 -0600

    agregando el operador logicon and

---
+++ EjerciciosVC/ejercicio-11-Operador Logico And/operadorAnd.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 50;
+    int b = 10;
+    int c = 7;
+
+// if(a > b && a > c)
+// { 
+//     cout <<"a es el mayor: " <<a;
+// }
+// else if (b > a && b > c)
+// {
+//    cout <<"b es el mayor: "<<b; 
+// }
+// else
+// {
+//     cout <<"c es el mayor"<<endl;
+// }
+
+    if(a > b && a > c)
+    { 
+        cout <<"a es el mayor: " <<a;
+    }
+    else if (b > a && b > c)
+    {
+    cout <<"b es el mayor: "<<b; 
+    }
+    else
+    {
+        cout <<"c es el mayor"<<endl;
+    }
+
+
+    return 0;
+}
