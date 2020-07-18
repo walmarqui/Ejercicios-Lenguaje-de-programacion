b6ab37f94d377c157de66e1ce54204e1eefeb062
walter <walmarqui@gmail.com>
2020-07-18 17:51:12 -0600

    ejercicio con ciclo while

---
+++ EjerciciosVC/ejercicio-16-Ciclos While/cicloWhile.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 5;
+    int b = 2;
+
+    while (a > b)
+    {
+        cout<<"Ingrese el valor de a ";
+        cin>>a;
+    }
+
+    cout<<endl;
+    cout<<"Saliste del ciclo ";
+    
+    return 0;
+}
