e2f502f0d465be77d5a807995808cc7d431f3061
walter <walmarqui@gmail.com>
2020-07-25 11:50:20 -0600

    ejercicio 04 todas las variables

---
+++ EjerciciosVC/ejercicio-04-imprimir el valor de todas las variables/todasLasVariables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorBoolean;
+    valorBoolean = true;
+
+    int valorEntero = 15;
+    double valorDouble = 20.99;
+    string valorString = "Hola como estas:";
+    char valorChar = 'B';
+
+    cout<<" Valor boolean: "<<valorBoolean<<endl;
+    cout<<" Valor entero: "<<valorEntero<<endl;
+    cout<<" Valor double: "<<valorDouble<<endl;
+    cout<<" Valor string: "<<valorString<<endl;
+    cout<<" Valor char: "<<valorChar<<endl;
+    
+    return 0;
+}
