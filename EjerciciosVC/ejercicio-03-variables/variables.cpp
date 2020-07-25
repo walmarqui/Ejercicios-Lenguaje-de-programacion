9435c059caad66ee88131afea3cc449473b1724e
walter <walmarqui@gmail.com>
2020-07-25 11:33:23 -0600

    agregando ejercicio 3 variables

---
+++ EjerciciosVC/ejercicio-03-variables/variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int numeroEntero;
+    numeroEntero = 15;
+
+    cout<<"numero entero "<<numeroEntero<<endl;
+
+    numeroEntero = 18;
+
+    cout<<"numero modificado: "<<numeroEntero;
+    return 0;
+}
