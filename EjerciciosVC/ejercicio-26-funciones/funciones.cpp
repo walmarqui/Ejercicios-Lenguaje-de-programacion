64393ad5848900bc82897223847eaa65deb1094d
walter <walmarqui@gmail.com>
2020-07-28 20:30:46 -0600

    primer ejercicio con funciones

---
+++ EjerciciosVC/ejercicio-26-funciones/funciones.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int sumar(){
+    return 5 + 7;
+}
+
+string nombreCompleto(){
+    string nombre = "juan";
+    string apellido = "Perez";
+    return nombre + " " + apellido;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    int resultado = sumar();
+    string nombreyApellido = nombreCompleto();
+
+    cout << resultado;
+    cout << endl;
+    cout << nombreyApellido;
+
+    return 0;
+}
