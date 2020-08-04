f2175adeb7f2c2eb2a8729e1fabc5767dbca538d
walter <walmarqui@gmail.com>
2020-08-03 19:14:23 -0600

    ejercicio punto de venta parte5

---
+++ EjerciciosVC/ejercicio-39-punto de venta parte5/productos.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+extern void agregarProducto(string descripcion, int cantidad, double precio);
+
+void productos(int opcion)
+{
+    system("cls");
+
+    int opcionProducto = 0;
+    switch (opcion)
+    {
+        case 1:
+        {
+            cout <<"BEBIDAS CALIENTES" << endl;
+            cout <<"*****************" << endl;
+            cout <<"1 - Capuccino" << endl;
+            cout <<"2 - Expresso" << endl;
+            cout << endl;
+
+            cout <<"Ingrese una opcion: ";
+            cin >> opcionProducto;
+
+            switch (opcionProducto)
+            {
+            case 1:
+                agregarProducto("1 capuccino", 1, 40);
+                break;
+            case 2:
+                agregarProducto("1 Expresso", 1, 30);
+                break;
+                
+            default:
+                {
+                cout <<" Opcion no valida";
+                return;
+                break;
+                }
+            }
+            cout << endl;
+            cout <<" Producto Agragado" << endl << endl;
+            system("pause");
+            
+            break;
+        }
+        case 2:
+        {
+            cout <<"BEBIDAS FRIAS" << endl;
+            cout <<"*************" << endl;
+            system("pause");
+            break;
+        }
+        case 3:
+        {
+            cout <<"REPOSTERIA" << endl;
+            cout <<"**********" << endl;
+            system("pause");
+            break;
+        }
+
+        default:
+        break;
+    }
+}
